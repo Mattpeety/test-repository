7ec2c408e2571f269b0c4aa3657eab735033d683
Roman Adamski <fakeemail@gmail.com>
2023-05-25 10:52:19 +0200

    feat: [TASK-445] my Intellij commit

--- Person.java
+++ Person.java
@@ -1,2 +1,10 @@
 public class Person {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+        int a = 3;
+        int b = 6;
+        int result = a + b;
+    }
 }
