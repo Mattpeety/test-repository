67213b38203934ce7eb21313611c2d274228f473
Roman Adamski <fakeemail@gmail.com>
2023-05-26 13:30:07 +0200

    feat: [TASK-01] Dog and Person modified

--- Person.java
+++ Person.java
@@ -1,10 +1,18 @@
 public class Person {
 
-
     public static void main(String[] args) {
         System.out.println("Hello!");
         int a = 3;
         int b = 6;
+        int asd = 123;
+        System.out.println("Some changes");
+        String abc = "a12345";
+
+        // jeeeeeeeeeeeeeeeeeeeeeeeeee
         int result = a + b;
+
+        public void someMethod() {
+
+        }
     }
 }
